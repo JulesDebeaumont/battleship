ad98ac9a7a1756a25f0f054cead47f0719c6485a
0824521 <0824521@chu-reims.fr>
2025-03-02 21:54:07 +0100

    [ADD] sdfjsdf

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { useUserStore } from 'src/stores/user-store';
-import { buttonRegularBind } from 'src/utils/binds';
-import { useRoute } from 'vue-router';
+import { useUserStore } from 'src/stores/user-store'
+import { buttonRegularBind } from 'src/utils/binds'
 
-const userStore = useUserStore();
-const route = useRoute();
+const userStore = useUserStore()
 </script>
 
 <template>
-    <q-page class="column q-px-md q-py-sm">
-        <div class="flex flex-center column">
-            <h6>Vous devez être connecté pour accéder à <i>Space Battle Ship</i></h6>
-            <q-btn v-bind="buttonRegularBind" label="Connexion" @click="userStore.askCasTicket" />
-        </div>
-    </q-page>
+  <q-page class="column q-px-md q-py-sm">
+    <div class="flex flex-center column">
+      <h6>Vous devez être connecté pour accéder à <i>Space Battle Ship</i></h6>
+      <q-btn v-bind="buttonRegularBind" label="Connexion" @click="userStore.askCasTicket" />
+    </div>
+  </q-page>
 </template>
